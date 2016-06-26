109e02d1915b0cba7976c81975f7a59f2d639d32
void <git.email.address@googlemail.com>
2016-06-26 23:54:00 +0200

    Did some refactoring to get rid of the ugly arguments

--- BadListener.Extension/BadListenerGenerator.cs
+++ BadListener.Extension/BadListenerGenerator.cs
@@ -17,9 +17,6 @@ namespace BadListener.Extension
     [ProvideObject(typeof(BadListenerGenerator))]
     public class BadListenerGenerator : IVsSingleFileGenerator
     {
-		MatchState _ModelPattern = new MatchState("^@model (.+)$");
-		MatchState _StatementPattern = new MatchState(@"^@(if|for|foreach|while\s*\(.+\))$");
-
         #region Implementation of interface IVsSingleFileGenerator
 
         int IVsSingleFileGenerator.DefaultExtension(out string pbstrDefaultExtension)
@@ -33,7 +30,8 @@ namespace BadListener.Extension
             try
             {
 				string viewName = Path.GetFileNameWithoutExtension(wszInputFilePath);
-                string code = GenerateCode(viewName, bstrInputFileContents, wszDefaultNamespace);
+				var generator = new CodeGenerator();
+                string code = generator.GenerateCode(viewName, bstrInputFileContents, wszDefaultNamespace);
                 var bytes = Encoding.UTF8.GetBytes(code);
                 int outputLength = bytes.Length;
                 rgbOutputFileContents[0] = Marshal.AllocCoTaskMem(outputLength);
@@ -61,106 +59,6 @@ namespace BadListener.Extension
 
         #endregion
 
-        private string GenerateCode(string viewName, string input, string @namespace)
-        {
-			var builder = new CodeBuilder();
-			builder.AppendLine("using BadListener;");
-			builder.AppendLine();
-			bool addNamespace = !string.IsNullOrEmpty(@namespace);
-			if (addNamespace)
-			{
-				builder.AppendLine($"namespace {@namespace}");
-				builder.IncreaseIndentation();
-			}
-			builder.AppendLine($"class {viewName} : View");
-			builder.IncreaseIndentation();
-			GenerateRenderCode(input, builder);
-			builder.DecreaseIndentation();
-			if (addNamespace)
-				builder.DecreaseIndentation();
-			string output = builder.ToString();
-			return output;
-        }
-
-		private void GenerateRenderCode(string input, CodeBuilder builder)
-		{
-			builder.IncreaseIndentation();
-			var lines = input.Split('\n');
-			var literals = new List<string>();
-			string model = null;
-			int lineCounter = 1;
-			foreach (string untrimmedLine in lines)
-			{
-				string line = untrimmedLine.Trim();
-				ProcessLine(line, lineCounter, builder, literals, ref model);
-				lineCounter++;
-			}
-			MergeAndEmitLiterals(literals, builder);
-			builder.DecreaseIndentation();
-			if (model == null)
-				throw new CompilerException("No model has been set.");
-			builder.PrependLine($"public override void Render({model} Model)");
-		}
-
-		private void ProcessLine(string line, int lineCounter, CodeBuilder builder, List<string> literals, ref string model)
-		{
-			if (line == "{")
-			{
-				MergeAndEmitLiterals(literals, builder);
-				builder.IncreaseIndentation();
-			}
-			else if (line == "}")
-			{
-				MergeAndEmitLiterals(literals, builder);
-				builder.DecreaseIndentation();
-			}
-			else if (line.Length > 0 && line[0] == '@')
-			{
-				if (_ModelPattern.Matches(line))
-				{
-					if (model != null)
-						throw new CompilerException("Encountered multiple model definitions.", lineCounter);
-					model = _ModelPattern.Group(1);
-				}
-				if (_StatementPattern.Matches(line))
-				{
-					string statement = _StatementPattern.Group(1);
-					builder.AppendLine(statement);
-				}
-				else
-				{
-					throw new CompilerException("Unknown statement.", lineCounter);
-				}
-			}
-			else
-			{
-				literals.Add(line);
-			}
-		}
-
-		private void MergeAndEmitLiterals(List<string> literals, CodeBuilder builder)
-		{
-			if (!literals.Any())
-				return;
-			string mergedLiterals = string.Join("\n", literals);
-			string escapedString = EscapeString(mergedLiterals);
-			builder.AppendLine($"Write(\"{escapedString}\");");
-			literals.Clear();
-		}
-
-		private string EscapeString(string input)
-		{
-			var replacements = new StringReplacement[]
-			{
-				new StringReplacement("\\", "\\\\"),
-				new StringReplacement("\r", "\\r"),
-				new StringReplacement("\n", "\\n"),
-				new StringReplacement("\"", "\\\""),
-			};
-			string output = input;
-			foreach (var replacement in replacements)
-				output = replacement.Execute(output);
-			return output;
-		}
+        
     }
 }
---
+++ BadListener.Extension/CodeGenerator.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BadListener.Extension
+{
+	class CodeGenerator
+	{
+		MatchState _ModelPattern = new MatchState("^@model (.+)$");
+		MatchState _StatementPattern = new MatchState(@"^@(if|for|foreach|while\s*\(.+\))$");
+
+		CodeBuilder _Builder;
+		string _Model;
+		List<string> _Literals;
+		int _LineCounter;
+
+		public string GenerateCode(string viewName, string input, string @namespace)
+		{
+			InitializeState();
+
+			_Builder.AppendLine("using BadListener;");
+			_Builder.AppendLine();
+			bool addNamespace = !string.IsNullOrEmpty(@namespace);
+			if (addNamespace)
+			{
+				_Builder.AppendLine($"namespace {@namespace}");
+				_Builder.IncreaseIndentation();
+			}
+			_Builder.AppendLine($"class {viewName} : View");
+			_Builder.IncreaseIndentation();
+			GenerateRenderCode(input);
+			_Builder.DecreaseIndentation();
+			if (addNamespace)
+				_Builder.DecreaseIndentation();
+			string output = _Builder.ToString();
+			return output;
+		}
+
+		private void InitializeState()
+		{
+			_Builder = new CodeBuilder();
+			_Model = null;
+			_Literals = new List<string>();
+			_LineCounter = 1;
+		}
+
+		private void GenerateRenderCode(string input)
+		{
+			_Builder.IncreaseIndentation();
+			var lines = input.Split('\n');
+			foreach (string untrimmedLine in lines)
+			{
+				string line = untrimmedLine.Trim();
+				ProcessLine(line);
+				_LineCounter++;
+			}
+			MergeAndEmitLiterals();
+			_Builder.DecreaseIndentation();
+			if (_Model == null)
+				throw new CompilerException("No model has been set.");
+			_Builder.PrependLine($"public override void Render({_Model} Model)");
+		}
+
+		private void ProcessLine(string line)
+		{
+			if (line == "{")
+			{
+				MergeAndEmitLiterals();
+				_Builder.IncreaseIndentation();
+			}
+			else if (line == "}")
+			{
+				MergeAndEmitLiterals();
+				_Builder.DecreaseIndentation();
+			}
+			else if (line.Length > 0 && line[0] == '@')
+			{
+				if (_ModelPattern.Matches(line))
+				{
+					if (_Model != null)
+						throw GetCompilerException("Encountered multiple model definitions.");
+					_Model = _ModelPattern.Group(1);
+				}
+				if (_StatementPattern.Matches(line))
+				{
+					string statement = _StatementPattern.Group(1);
+					_Builder.AppendLine(statement);
+				}
+				else
+				{
+					throw GetCompilerException("Unknown statement.");
+				}
+			}
+			else
+			{
+				_Literals.Add(line);
+			}
+		}
+
+		private void MergeAndEmitLiterals()
+		{
+			if (!_Literals.Any())
+				return;
+			string mergedLiterals = string.Join("\n", _Literals);
+			string escapedString = EscapeString(mergedLiterals);
+			_Builder.AppendLine($"Write(\"{escapedString}\");");
+			_Literals.Clear();
+		}
+
+		private string EscapeString(string input)
+		{
+			var replacements = new StringReplacement[]
+			{
+				new StringReplacement("\\", "\\\\"),
+				new StringReplacement("\r", "\\r"),
+				new StringReplacement("\n", "\\n"),
+				new StringReplacement("\"", "\\\""),
+			};
+			string output = input;
+			foreach (var replacement in replacements)
+				output = replacement.Execute(output);
+			return output;
+		}
+
+		private CompilerException GetCompilerException(string message)
+		{
+			return new CompilerException(message, _LineCounter);
+		}
+	}
+}
